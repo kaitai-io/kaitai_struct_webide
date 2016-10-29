840825e33562590408ced273ec658aaac580bc2a
KT <koczkatamas@gmail.com>
2016-10-30 01:11:36 +0200

    Add HexViewer

---
+++ js/HexViewer.js
@@ -0,0 +1,142 @@
+class HexViewUtils {
+    static zeroFill(str, padLen) {
+        while (str.length < padLen)
+            str = '0' + str;
+        return str;
+    }
+    static addrHex(address) {
+        //var addrHexLen = Math.ceil(Math.log(this.buffer.length) / Math.log(16));
+        var addrHexLen = 8;
+        return this.zeroFill(address.toString(16), addrHexLen);
+    }
+    static byteAscii(bt) {
+        return bt == 32 ? '\u00a0' : bt < 32 || (0x7f <= bt && bt <= 0xa0) || bt == 0xad ? '.' : String.fromCharCode(bt);
+    }
+    static byteHex(bt) {
+        return this.zeroFill(bt.toString(16), 2);
+    }
+    static generateRow(bytesPerLine, level) {
+        level = level || 3;
+        function cr(tag, className) {
+            var elem = document.createElement(tag);
+            elem.className = className;
+            return elem;
+        }
+        var hexRow = cr('div', 'hexRow');
+        hexRow.addrPart = cr('span', 'addrPart');
+        hexRow.hexPart = cr('span', 'hexPart');
+        hexRow.asciiPart = cr('span', 'asciiPart');
+        hexRow.appendChild(hexRow.addrPart);
+        hexRow.appendChild(hexRow.hexPart);
+        hexRow.appendChild(hexRow.asciiPart);
+        for (var iChar = 0; iChar < bytesPerLine; iChar++) {
+            hexRow.asciiPart.appendChild(cr('span', `asciicell cell${iChar}`));
+            var cell = cr('span', `hexcell cell${iChar}`);
+            var levels = [];
+            var prevLevel = cell;
+            for (var i = 0; i < level; i++) {
+                var levelSpan = cr('span', `l${i}`);
+                levelSpan.appendChild(prevLevel);
+                levels[level - 1 - i] = prevLevel = levelSpan;
+            }
+            prevLevel.cell = cell;
+            prevLevel.levels = levels;
+            hexRow.hexPart.appendChild(prevLevel);
+        }
+        return hexRow;
+    }
+}
+class HexViewer {
+    constructor(containerId, dataProvider) {
+        this.dataProvider = dataProvider;
+        this.bytesPerLine = 16;
+        this.rows = [];
+        this.topRow = 0;
+        this.maxLevel = 3;
+        var rowHeight = 21;
+        var scrollbox = $('#' + containerId);
+        scrollbox.addClass('hexViewer');
+        var heightbox = $('<div class="heightbox"></div>').appendTo(scrollbox);
+        var content = $('<div class="content"></div>').appendTo(scrollbox);
+        var totalRowCount = Math.ceil(dataProvider.length / this.bytesPerLine);
+        var totalHeight = totalRowCount * rowHeight;
+        heightbox.height(totalHeight);
+        var maxScrollHeight, maxRow;
+        this.intervals = [];
+        scrollbox.on('scroll', e => {
+            var scrollTop = scrollbox.scrollTop();
+            content.css('top', scrollTop + 'px');
+            var percent = scrollTop / maxScrollHeight;
+            var newTopRow = Math.round(maxRow * percent);
+            console.log(scrollTop, percent);
+            if (this.topRow !== newTopRow) {
+                this.topRow = newTopRow;
+                this.refresh();
+            }
+        });
+        var resize = () => {
+            var boxHeight = scrollbox.height();
+            content.height(boxHeight + 'px');
+            content.html('');
+            maxScrollHeight = totalHeight - boxHeight;
+            this.rowCount = Math.ceil(boxHeight / rowHeight);
+            maxRow = Math.ceil(dataProvider.length / this.bytesPerLine - this.rowCount + 1);
+            this.rows = [];
+            for (var i = 0; i < this.rowCount; i++) {
+                var row = HexViewUtils.generateRow(this.bytesPerLine, this.maxLevel);
+                this.rows[i] = row;
+                content.append(row);
+            }
+            this.refresh();
+        };
+        $(window).on('resize', () => resize());
+        resize();
+    }
+    refresh() {
+        var startOffset = this.topRow * this.bytesPerLine;
+        var intIdx;
+        for (intIdx = 0; intIdx < this.intervals.length; intIdx++)
+            if (this.intervals[intIdx].start <= startOffset && startOffset <= this.intervals[intIdx].end)
+                break;
+        var data = this.dataProvider.get(startOffset, Math.min(this.rowCount * this.bytesPerLine, this.dataProvider.length - startOffset));
+        for (var iRow = 0; iRow < this.rowCount; iRow++) {
+            var rowOffset = iRow * this.bytesPerLine;
+            var row = this.rows[iRow];
+            row.addrPart.innerText = rowOffset < data.length ? HexViewUtils.addrHex(startOffset + rowOffset) : '';
+            for (var iCell = 0; iCell < this.bytesPerLine; iCell++) {
+                var dataOffset = rowOffset + iCell;
+                var cellOffset = startOffset + dataOffset;
+                var hexCh, ch;
+                if (dataOffset < data.length) {
+                    var b = data[rowOffset + iCell];
+                    hexCh = HexViewUtils.byteHex(b);
+                    ch = HexViewUtils.byteAscii(b);
+                }
+                else {
+                    hexCh = '\u00a0\u00a0';
+                    ch = '\u00a0';
+                }
+                var hexCell = row.hexPart.childNodes[iCell];
+                hexCell.cell.innerText = hexCh;
+                row.asciiPart.childNodes[iCell].innerText = ch;
+                var skipInt = 0;
+                for (var level = 0; level < this.maxLevel; level++) {
+                    var int = this.intervals[intIdx + level];
+                    var intIn = int && int.start <= cellOffset && cellOffset <= int.end;
+                    var intStart = intIn && int.start === cellOffset;
+                    var intEnd = intIn && int.end === cellOffset;
+                    hexCell.levels[level].className = `l${this.maxLevel - 1 - level}` +
+                        (intIn ? ` m${level}` : "") + (intStart ? " start" : "") + (intEnd ? " end" : "");
+                    if (intEnd)
+                        skipInt++;
+                }
+                intIdx += skipInt;
+            }
+        }
+    }
+    setIntervals(intervals) {
+        this.intervals = intervals;
+        this.refresh();
+    }
+}
+//# sourceMappingURL=HexViewer.js.map
--- js/app.js
+++ js/app.js
@@ -7,5 +7,39 @@ $(() => {
         ksyEditor.gotoLine(0);
         console.log('ajax done, len: ', ksyContent.length);
     });
+    function downloadFile(url) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'arraybuffer';
+        return new Promise((resolve, reject) => {
+            xhr.onload = function (e) {
+                resolve(new Uint8Array(this.response));
+            };
+            xhr.send();
+        });
+    }
+    downloadFile('/samples/pnggrad8rgb.png').then(pngContent => {
+        var dataProvider = {
+            length: pngContent.length,
+            get(offset, length) {
+                var res = [];
+                for (var i = 0; i < length; i++)
+                    res.push(pngContent[offset + i]); // TODO: use ArrayBuffer
+                return res;
+            }
+        };
+        var hexViewer = new HexViewer("hexViewer", dataProvider);
+        hexViewer.setIntervals([
+            { start: 0, end: 2 },
+            { start: 1, end: 2 },
+            { start: 2, end: 2 },
+            { start: 3, end: 6 },
+            { start: 7, end: 10 },
+            { start: 16 + 0, end: 16 + 8 },
+            { start: 16 + 1, end: 16 + 8 },
+            { start: 16 + 2, end: 16 + 8 },
+            { start: 32, end: 95 },
+        ]);
+    });
 });
 //# sourceMappingURL=app.js.map
