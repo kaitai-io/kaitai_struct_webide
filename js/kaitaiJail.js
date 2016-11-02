d480323659bd51eaea738ce934d320b089f9d7a7
KT <koczkatamas@gmail.com>
2016-11-02 22:48:47 +0100

    Started adding instance support (no GUI yet)

--- js/kaitaiJail.js
+++ js/kaitaiJail.js
@@ -1,4 +1,20 @@
-﻿// executes the given code and handles the result
+﻿function toExport(obj, name) {
+    if (typeof obj != "object") return obj;
+
+    var result = {};
+    result._debug = obj._debug || {};
+    result._debug.class = obj.constructor.name;
+    var props = Object.getOwnPropertyNames(obj.constructor.prototype).filter(x => x[0] != '_' && x != "constructor");
+    console.log('keys', Object.keys(obj), 'props', props);
+    Object.keys(obj).filter(x => x[0] != '_').forEach(key => result[key] = toExport(obj[key], name.concat(key)));
+    props.forEach(key => {
+        if (!('_props' in result))
+            result._props = {};
+        result._props[key] = name.concat(key);
+    });
+    return result;
+}
+
 application.setInterface({
     run: function (code, args, cb) {
         var result = { input: code, output: null, error: null };
@@ -22,22 +38,18 @@ application.setInterface({
             parseError = e;
         }
         
-
-        function prepareForExport(obj) {
-            if(typeof obj != "object") return;
-
-            delete obj._io;
-            delete obj._root;
-            delete obj._parent;
-            if (obj._debug)
-                obj._debug.class = obj.constructor.name;
-
-            Object.keys(obj).forEach(key => prepareForExport(obj[key]));
+        exported = toExport(parsed, []);
+        console.log('[jail] parsed', parsed, 'exported', exported);
+        cb(exported, parseError);
+    },
+    get: function (path, cb) {
+        var obj = parsed;
+        try {
+            path.forEach(key => obj = obj[key]);
+        } catch (e) {
+            console.log(e);
+            parseError = e;
         }
-
-        console.log('parsed in jail', parsed);
-
-        prepareForExport(parsed);
-        cb(parsed, parseError);
+        cb(obj, parseError);
     }
 });
--- src/app.ts
+++ src/app.ts
@@ -61,7 +61,7 @@ function addEditor(name: string, lang: string, isReadOnly: boolean = false) {
 
 addEditor('ksyEditor', 'yaml');
 addEditor('genCodeViewer', 'javascript', true);
-addEditor('genCodeDebugViewer', 'javascript', true);
+addEditor('genCodeDebugViewer', 'javascript', false);
 addEditor('parsedDataViewer', 'javascript', true);
 addComponent('hexViewer', () => new HexViewer("hexViewer"));
 addComponent('errorWindow', cont => { cont.getElement().append($("<div />")); return cont; });
@@ -112,6 +112,12 @@ $(() => {
 
     ui.hexViewer.onSelectionChanged();
 
+    ui.genCodeDebugViewer.commands.addCommand({
+        name: "compile",
+        bindKey: { win: "Ctrl-Enter", mac: "Command-Enter" },
+        exec: function (editor) { reparse(); }
+    });
+
     var lineInfo = null;
     ui.parsedDataViewer.getSession().selection.on('changeCursor', (e1, e2) => {
         var lineIdx = e2.selectionLead.row;
@@ -144,110 +150,120 @@ $(() => {
 
         ui.genCodeViewer.setValue(r[0], -1);
         ui.genCodeDebugViewer.setValue(rDebug[0], -1);
-        jailrun(`module = { exports: true }; \n ${rDebug[0]} \n`).then(reparse);
-        console.log('recompiled');
+        reparse();
     }
 
     function reparse() {
-        return Promise.all([jailReady, inputReady, formatReady]).then(() => jail.remote.reparse((res, error) => {
-            if (error)
-                showError(`Parse error (${error.name}): ${error.message}\nCall stack: ${error.stack}`, error);
-            else
-                hideErrors();
+        return Promise.all([jailReady, inputReady, formatReady]).then(() => {
+            var debugCode = ui.genCodeDebugViewer.getValue();
+            return jailrun(`module = { exports: true }; \n ${debugCode} \n`);
+        }).then(() => {
+            console.log('recompiled');
 
-            var intervals = [];
-            var padLen = 2;
-            var commentOffset = 60;
+            jail.remote.reparse((res, error) => {
+                window['parseRes'] = res;
+                console.log('reparse res', res);
 
-            function commentPad(str) { return str.length < commentOffset ? str + ' '.repeat(commentOffset - str.length) : str; }
+                if (error)
+                    showError(`Parse error (${error.name}): ${error.message}\nCall stack: ${error.stack}`, error);
+                else
+                    hideErrors();
 
-            lineInfo = { currLine: 0, lineStart: 0, lines: {} };
-            var json = "";
+                var intervals = [];
+                var padLen = 2;
+                var commentOffset = 60;
 
-            function nl() {
-                json += "\n";
-                lineInfo.currLine++;
-                lineInfo.lineStart = json.length;
-            }
+                function commentPad(str) { return str.length < commentOffset ? str + ' '.repeat(commentOffset - str.length) : str; }
 
-            function comment(str) {
-                var padLen = commentOffset - (json.length - lineInfo.lineStart);
-                if (padLen > 0)
-                    json += ' '.repeat(padLen);
-                json += ` // ${str}`;
-            }
+                lineInfo = { currLine: 0, lineStart: 0, lines: {} };
+                var json = "";
 
-            function getLenComment(debug, addInterval = false) {
-                if (debug) {
-                    var len = debug.end - debug.start + 1;
-                    if (len > 0)
-                        intervals.push(debug);
-                    lineInfo.lines[lineInfo.currLine] = debug;
-                    return `${debug.start}-${debug.end} (l:${len})`;
+                function nl() {
+                    json += "\n";
+                    lineInfo.currLine++;
+                    lineInfo.lineStart = json.length;
                 }
 
-                return null;
-            }
+                function comment(str) {
+                    var padLen = commentOffset - (json.length - lineInfo.lineStart);
+                    if (padLen > 0)
+                        json += ' '.repeat(padLen);
+                    json += ` // ${str}`;
+                }
 
-            function toJson(obj, debug = null, pad = 0) {
-                //debug = debug || obj._debug;
-                if (typeof obj === "object") {
-                    var objPad = " ".repeat((pad + 0) * padLen);
-                    var childPad = " ".repeat((pad + 1) * padLen);
-                    if (obj instanceof Uint8Array) {
-                        json += "[";
-                        for (var i = 0; i < obj.length; i++) {
-                            json += i == 0 ? "" : ", ";
-                            if (i != 0 && (i % 8 == 0)) {
-                                nl();
-                                json += childPad;
+                function getLenComment(debug, addInterval = false) {
+                    if (debug) {
+                        var len = debug.end - debug.start + 1;
+                        if (len > 0)
+                            intervals.push(debug);
+                        lineInfo.lines[lineInfo.currLine] = debug;
+                        return `${debug.start}-${debug.end} (l:${len})`;
+                    }
+
+                    return null;
+                }
+
+                function toJson(obj, debug = null, pad = 0) {
+                    //debug = debug || obj._debug;
+                    if (typeof obj === "object") {
+                        var objPad = " ".repeat((pad + 0) * padLen);
+                        var childPad = " ".repeat((pad + 1) * padLen);
+                        if (obj instanceof Uint8Array) {
+                            json += "[";
+                            for (var i = 0; i < obj.length; i++) {
+                                json += i == 0 ? "" : ", ";
+                                if (i != 0 && (i % 8 == 0)) {
+                                    nl();
+                                    json += childPad;
+                                }
+                                json += obj[i];
                             }
-                            json += obj[i];
-                        }
-                        json += "]";
-                    } else {
-                        var keys = Object.keys(obj).filter(x => x != "_debug");
-                        var isArray = Array.isArray(obj);
-                        json += isArray ? `[` : `{`;
-                        if (!isArray) {
-                            json += ` // ${obj._debug.class}`;
-                            if(debug)
-                                comment(getLenComment(debug));
-                        }
+                            json += "]";
+                        } else {
+                            var keys = Object.keys(obj).filter(x => x[0] != "_");
+                            var isArray = Array.isArray(obj);
+                            json += isArray ? `[` : `{`;
+                            if (!isArray) {
+                                if (obj._debug)
+                                    json += ` // ${obj._debug.class}`;
+                                if (debug)
+                                    comment(getLenComment(debug));
+                            }
 
-                        for (var i = 0; i < keys.length; i++) {
-                            var key = keys[i];
+                            for (var i = 0; i < keys.length; i++) {
+                                var key = keys[i];
+
+                                nl();
+                                json += childPad + (isArray ? "" : `"${key}": `);
+
+                                var childDebug = isArray ? debug.arr[key] : obj._debug ? obj._debug[key] : null;
+                                var isObject = toJson(obj[key], childDebug, pad + 1);
+
+                                json += (i == keys.length - 1 ? "" : ",");
+
+                                if (!isObject)
+                                    comment(' ' + getLenComment(childDebug, true));
+                            }
 
                             nl();
-                            json += childPad + (isArray ? "" : `"${key}": `);
-
-                            var childDebug = isArray ? debug.arr[key] : obj._debug ? obj._debug[key] : null;
-                            var isObject = toJson(obj[key], childDebug, pad + 1);
-
-                            json += (i == keys.length - 1 ? "" : ",");
-
-                            if (!isObject)
-                                comment(' ' + getLenComment(childDebug, true));
+                            json += objPad + (isArray ? `]` : `}`);
+                            return true;
                         }
-
-                        nl();
-                        json += objPad + (isArray ? `]` : `}`);
-                        return true;
                     }
+                    else if (typeof obj === "number")
+                        json += `${obj}`;
+                    else
+                        json += `"${obj}"`;
+
+                    return false;
                 }
-                else if(typeof obj === "number")
-                    json += `${obj}`;
-                else
-                    json += `"${obj}"`;
 
-                return false;
-            }
-
-            toJson(res);
-            console.log(lineInfo);
-            ui.parsedDataViewer.setValue(json);
-            ui.hexViewer.setIntervals(intervals);
-        }));
+                toJson(res);
+                console.log(lineInfo);
+                ui.parsedDataViewer.setValue(json);
+                ui.hexViewer.setIntervals(intervals);
+            });
+        });
     }
 
     jail = new jailed.Plugin(baseUrl + 'js/kaitaiJail.js');
@@ -257,7 +273,7 @@ $(() => {
     }).then(() => new Promise((resolve, reject) => jail._connection.importScript(baseUrl + 'lib/kaitai_js_runtime/KaitaiStream.js', resolve, reject)));
     jailReady.then(() => console.log('jail started'), () => console.log('jail fail'));
 
-    var inputReady = downloadFile('samples/sample1.zip').then(fileBuffer => {
+    var inputReady = downloadFile('samples/grad8rgb.bmp').then(fileBuffer => {
         var fileContent = new Uint8Array(fileBuffer);
         var dataProvider = {
             length: fileContent.length,
@@ -274,7 +290,7 @@ $(() => {
         return jailrun('inputBuffer = args; void(0)', fileBuffer);
     });
 
-    var formatReady = Promise.resolve($.ajax({ url: 'formats/archive/zip.ksy' })).then(ksyContent => {
+    var formatReady = Promise.resolve($.ajax({ url: 'formats/image/bmp.ksy' })).then(ksyContent => {
         ui.ksyEditor.setValue(ksyContent, -1);
         var editDelay = new Delayed(500);
         ui.ksyEditor.on('change', () => editDelay.do(() => recompile()));
