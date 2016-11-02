c45dfd918c32cc0ce587f99f8aacdf10c7afcfa7
Mikhail Yakshin <greycat@altlinux.org>
2016-11-02 14:36:48 +0300

    Fixed whitespace, end-of-file newline and added shebang

--- serve.py
+++ serve.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import SocketServer, SimpleHTTPServer, json, glob, os, sys, threading, time, subprocess
 
 PORT = 8000
@@ -43,12 +45,12 @@ def compileThread():
                 print "Compile errors:%s" % ('\n' + e.output.strip()).replace('\n', '\n -  ')
             finally:
                 compileInProgress = False
-            
+
 if '--compile' in sys.argv:
     t = threading.Thread(target=compileThread)
     t.setDaemon(True)
     t.start()
     print "auto compile started"
-            
+
 print "please use 127.0.0.1:%d on Windows (using localhost makes 1sec delay)" % PORT
 httpd = SocketServer.TCPServer(("", PORT), MyHandler).serve_forever()
