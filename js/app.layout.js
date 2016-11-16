18f13f5519c014bb3458e8e4040324500313dcf1
KT <koczkatamas@gmail.com>
2016-11-16 13:02:38 +0100

    Add compiled version of previous pull request changes

--- js/app.layout.js
+++ js/app.layout.js
@@ -59,6 +59,8 @@ function addEditor(name, lang, isReadOnly = false, callback = null) {
         var editor = ace.edit(container.getElement().get(0));
         editor.setTheme("ace/theme/monokai");
         editor.getSession().setMode(`ace/mode/${lang}`);
+        if (lang === 'yaml')
+            editor.setOption('tabSize', 2);
         editor.$blockScrolling = Infinity; // TODO: remove this line after they fix ACE not to throw warning to the console
         editor.setReadOnly(isReadOnly);
         if (callback)
