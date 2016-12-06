e9fa32b85620aaf8356f18f566a8083b6516063d
KT <koczkatamas@gmail.com>
2016-12-06 21:46:28 +0100

    Refresh format gallery

--- genKaitaiFsFiles.py
+++ genKaitaiFsFiles.py
@@ -11,4 +11,6 @@ def recursive_glob(treeroot, pattern):
         results.extend(os.path.join(base, f) for f in goodfiles)
     return results
     
-print 'var files = [\n' + '\n'.join("    '"+x.replace('\\','/')+"'," for x in recursive_glob('formats/', '*.ksy') + recursive_glob('samples/', '*')) + '\n];';
+js = 'var kaitaiFsFiles = [\n' + '\n'.join("    '"+x.replace('\\','/')+"'," for x in recursive_glob('formats/', '*.ksy') + recursive_glob('samples/', '*')) + '\n];';
+with open('js/kaitaiFsFiles.js', 'wt') as f: f.write(js)
+print js
