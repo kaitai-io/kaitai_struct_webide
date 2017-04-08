0ccc5526abc745b3fdd230e7206a1507be6f547f
KT <koczkatamas@gmail.com>
2017-04-09 00:12:49 +0200

    [sandbox] Add RemoteFS implementation + server

---
+++ src/FileSystem/RemoteFs.ts
@@ -0,0 +1,74 @@
+﻿import { FsUri } from './FsUri';
+import { IFileSystem, IFsItem } from './Interfaces';
+
+interface IListResponse {
+    files: [{ fn: string, isDir: boolean }];
+}
+
+class RemoteFsItem implements IFsItem {
+    constructor(public uri: FsUri) { }
+}
+
+export class RemoteFileSystem implements IFileSystem {
+    public mappings: { [path: string]: { secret: string } } = {};
+
+    private getFsUri(uri: string) { return new FsUri(uri, 2); }
+
+    request(method: string, url: string, headers?: { [name: string]: string }, responseType?: string, requestData?: Blob|ArrayBuffer) {
+        return new Promise((resolve, reject) => {
+            var xhr = new XMLHttpRequest();
+            xhr.open(method, url, true);
+            if (responseType)
+                xhr.responseType = responseType;
+
+            if(headers)
+                for (var hdrName in headers)
+                    xhr.setRequestHeader(hdrName, headers[hdrName]);
+
+            xhr.onload = e => {
+                if (200 <= xhr.status && xhr.status <= 299) {
+                    var contentType = xhr.getResponseHeader('content-type');
+                    if (contentType === 'application/json' && !responseType)
+                        resolve(JSON.parse(xhr.response));
+                    else
+                        resolve(xhr.response);
+                }
+                else
+                    reject(xhr.response);
+            };
+
+            xhr.onerror = e => reject(e);
+            xhr.send(requestData);
+        });
+    }
+
+    execute<T>(method: string, uri: string, binaryResponse: boolean = false, postData: ArrayBuffer = null): Promise<T> {
+        var fsUri = this.getFsUri(uri);
+        var host = fsUri.providerData[0];
+        if (host.indexOf(':') === -1)
+            host += '8001';
+        var mapping = fsUri.providerData[1] || 'default';
+        var mappingConfig = this.mappings[`${host}/${mapping}`];
+
+        var url = `http://${host}/files/${mapping}${fsUri.path}`;
+        return this.request(method, url, { 'Authorization': 'MappingSecret ' + mappingConfig.secret }, binaryResponse ? 'arraybuffer' : null, postData);
+    }
+
+    read(uri: string): Promise<ArrayBuffer> {
+        return this.execute('GET', uri, true);
+    }
+
+    write(uri: string, data: ArrayBuffer): Promise<void> {
+        return this.execute('PUT', uri, false, data).then(x => null);
+    }
+
+    delete(uri: string): Promise<void> {
+        return this.execute('DELETE', uri).then(x => null);
+    }
+
+    list(uri: string): Promise<RemoteFsItem[]> {
+        return this.execute('GET', uri).then(response => {
+            return (<IListResponse>response).files.map(item => new RemoteFsItem(this.getFsUri(uri + item.fn + (item.isDir ? '/' : ''))));
+        });
+    }
+}
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -3,8 +3,8 @@ import { GithubFileSystem } from './FileSystem/GithubFs';
 import { IFileSystem, IFsItem } from './FileSystem/Interfaces';
 import { FsUri } from './FileSystem/FsUri';
 import { LocalFileSystem } from './FileSystem/LocalFs';
-
-var queryParams: { access_token?: string } = {};
+import { RemoteFileSystem } from './FileSystem/RemoteFs';
+var queryParams: { access_token?: string, secret?: string } = {};
 location.search.substr(1).split('&').map(x => x.split('=')).forEach(x => queryParams[x[0]] = x[1]);
 
 var githubClient = new GithubClient(queryParams.access_token);
@@ -22,9 +22,14 @@ function dataToArrayBuffer(str) {
 var localFs = new LocalFileSystem();
 //localFs.write('local:///file.txt', dataToArrayBuffer("file content"));
 //localFs.write('local:///folder/file.txt', dataToArrayBuffer("file content"));
-localFs.list('local:///folder/').then(x => console.log(x));
-localFs.read('local:///folder/file.txt').then(x => console.log(x));
+//localFs.list('local:///folder/').then(x => console.log(x));
+//localFs.read('local:///folder/file.txt').then(x => console.log(x));
 
+var remoteFs = new RemoteFileSystem();
+remoteFs.mappings["127.0.0.1:8001/default"] = { secret: queryParams.secret };
+remoteFs.list('remote://127.0.0.1:8001/default/folder/').then(x => console.log(x));
+remoteFs.read('remote://127.0.0.1:8001/default/test.ksy').then(x => console.log(x, typeof x));
+remoteFs.write('remote://127.0.0.1:8001/default/test.ksy', dataToArrayBuffer("file content"));
 
 //githubFs.list('github://koczkatamas/kaitai_struct_formats/archive/').then(items => console.log(items.map((x: GithubFsItem) => `${x.uri.uri}`)));
 //githubFs.read('github://koczkatamas/kaitai_struct_formats/archive/zip.ksy').then(result => console.log(result));
