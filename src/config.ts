efddf71a7add2bb6cfe96443f54f43ad88d07b45
KT <koczkatamas@gmail.com>
2017-04-15 23:30:54 +0200

    Test Knockout

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 var kaitaiIde = kaitaiIde || {};
-kaitaiIde.version = '0.1.0.1123';
+kaitaiIde.version = '0.1.0.1167';
--- src/sandbox.ts
+++ src/sandbox.ts
@@ -1,22 +1,15 @@
-﻿import { GithubClient } from './FileSystem/GithubClient';
+﻿///<reference path="../lib/ts-types/knockout.d.ts"/>
+import { GithubClient } from './FileSystem/GithubClient';
 import { GithubFileSystem } from './FileSystem/GithubFs';
 import { IFileSystem, IFsItem } from './FileSystem/Interfaces';
 import { FsUri } from './FileSystem/FsUri';
 import { LocalFileSystem } from './FileSystem/LocalFs';
 import { RemoteFileSystem } from './FileSystem/RemoteFs';
 import { FsSelector } from './FileSystem/FsSelector';
+import * as ko from "knockout";
 
 var queryParams: { access_token?: string, secret?: string } = {};
 location.search.substr(1).split('&').map(x => x.split('=')).forEach(x => queryParams[x[0]] = x[1]);
-
-function dataToArrayBuffer(str) {
-    var len = str.length;
-    var bytes = new Uint8Array(len);
-    for (var i = 0; i < len; i++) {
-        bytes[i] = str.charCodeAt(i);
-    }
-    return bytes.buffer;
-}
 
 var fs = new FsSelector();
 fs.addFs(new LocalFileSystem());
@@ -29,5 +22,49 @@ var githubClient = new GithubClient(queryParams.access_token);
 var githubFs = new GithubFileSystem(githubClient);
 fs.addFs(githubFs);
 
-['local:///folder/', 'remote://127.0.0.1:8001/default/folder/', 'github://koczkatamas/kaitai_struct_formats/archive/']
-    .forEach(uri => fs.list(uri).then(items => console.log(items.map(item => `${item.uri.uri} (${item.uri.type})`))));
+console.log(ko);
+
+var viewModel = {
+    treeRoot: ko.observableArray()
+};
+
+class TreeElement {
+    public name: KnockoutObservable<string>;
+    public children: KnockoutObservableArray<TreeElement>;
+    public isOpen: KnockoutObservable<boolean>;
+
+    constructor(name: string, children: TreeElement[] = []) {
+        this.name = ko.observable(name);
+        this.isOpen = ko.observable(false);
+        this.children = ko.observableArray(children);
+    }
+
+    openCloseNode() {
+        this.isOpen(!this.isOpen());
+    }
+}
+
+var tree = [
+    new TreeElement("Russia", [
+        new TreeElement("Moscow")
+    ]),
+    new TreeElement("Germany"),
+    new TreeElement("United States",
+        [
+            new TreeElement("Atlanta"),
+            new TreeElement("New York", [
+                new TreeElement("Harlem"),
+                new TreeElement("Central Park")
+            ])
+        ]),
+    new TreeElement("Canada", [
+        new TreeElement("Toronto2")
+    ])
+];
+
+viewModel.treeRoot(tree);
+ko.applyBindings(viewModel);
+
+//['local:///folder/', 'remote://127.0.0.1:8001/default/folder/', 'github://koczkatamas/kaitai_struct_formats/archive/']
+//    .forEach(uri => fs.list(uri).then(items => console.log(items.map(item => `${item.uri.uri} (${item.uri.type})`))));
+
