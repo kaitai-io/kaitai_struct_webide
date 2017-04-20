2462199949a66a6ab27c3db111ae3fa0393828bd
KT <koczkatamas@gmail.com>
2017-04-20 20:02:16 +0200

    Add GA while build, separate GA for devel

---
+++ build.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python2
+import os
+import sys
+
+GA_TEMPLATE = '''
+    <!-- Google Analytics -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','_ga');
+
+        _ga('create', '#GA_ID#', 'auto');
+        _ga('send', 'pageview');
+        function ga(category, action, label, value) {
+            console.log(`[GA Event] cat:${category} act:${action} lab:${label || ''}`);
+            _ga('send', 'event', category, action, label, value);
+        }
+    </script>
+    <!-- End Google Analytics --> 
+'''
+
+outDir = sys.argv[1] if len(sys.argv) > 1 else 'out'
+gaId = os.environ.get('GA_ID')
+
+def fileAction(fn, action):
+    with open(fn, 'rt') as f: content = f.read()
+    newContent = action(content)
+    with open(fn, 'wt') as f: f.write(newContent)
+    
+def appendAfter(str, afterStr, appendStr):
+    i = str.index(afterStr) + len(afterStr)
+    return str[0:i] + appendStr + str[i:]
+
+if gaId:
+    fileAction(outDir + '/index.html', lambda html: appendAfter(html, '</title>', GA_TEMPLATE.rstrip().replace('#GA_ID#', gaId)))
