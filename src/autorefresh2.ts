0bb529b69555af2ec53d7abcc531b4000d7fce4a
KT <koczkatamas@gmail.com>
2017-06-25 22:16:22 +0200

    Remove jQuery dep from autorefresh2

--- src/autorefresh2.ts
+++ src/autorefresh2.ts
@@ -1,8 +1,5 @@
-import * as $ from "jquery";
-
 function checkModifications() {
-    $.getJSON("/onchange", () => location.reload(true))
-        .fail(() => setTimeout(checkModifications, 750));
+    fetch("/onchange").then(_ => location.reload(true), _ => setTimeout(checkModifications, 750));
 }
 
 if(location.hostname === "127.0.0.1")
