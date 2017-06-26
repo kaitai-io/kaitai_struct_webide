ad499ba339ebb77d8653c1876608e9637ad53293
KT <koczkatamas@gmail.com>
2017-06-26 20:06:52 +0200

    Fix autorefresh2 on connection error

--- src/autorefresh2.ts
+++ src/autorefresh2.ts
@@ -1,5 +1,5 @@
 function checkModifications2() {
-    fetch("/onchange").then(_ => location.reload(true), _ => setTimeout(checkModifications, 750));
+    fetch("/onchange").then(_ => location.reload(true), _ => setTimeout(checkModifications2, 750));
 }
 
 if(location.hostname === "127.0.0.1")
--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,2 @@
 var kaitaiIde: any = kaitaiIde || {};
-kaitaiIde.version = "0.1.0.4356";
+kaitaiIde.version = "0.1.0.4357";
