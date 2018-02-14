8f63b2ce42df2d8783f4b6bebbbf17f09257d46b
KT <koczkatamas@gmail.com>
2018-02-14 20:05:19 +0100

    Allow >= Opera 50 as supported browser

--- src/v1/app.unsupportedBrowser.ts
+++ src/v1/app.unsupportedBrowser.ts
@@ -1,6 +1,6 @@
 ﻿import * as bowser from "bowser";
 
-if (localStorage.getItem("hideUnsupported") === "true" || bowser.check({ chrome: "55", firefox: "52", safari: "10.1" }, true))
+if (localStorage.getItem("hideUnsupported") === "true" || bowser.check({ chrome: "55", firefox: "52", safari: "10.1", opera: "50" }, true))
     document.getElementById("unsupportedBrowser").style.display = "none";
 
 document.querySelector("#unsupportedBrowser .closeBtn").addEventListener("click", () => {
