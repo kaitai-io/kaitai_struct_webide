5e52a5ad8d2c4bbbb92f5fb9037ddb7f633f00e8
John Chadwick <johnwchadwick@gmail.com>
2019-08-26 22:17:34 -0700

    Reimplement vendor_build.py and vendor_license.py in JS.

---
+++ vendor_build.js
@@ -0,0 +1,59 @@
+const YAML = require("yamljs");
+const { copyFileSync, readdirSync, statSync, mkdirSync } = require("fs");
+const { join, basename, dirname } = require("path");
+const firstBy = require("thenby");
+
+function isDirectory(file) {
+    return statSync(file).isDirectory();
+}
+
+function copyOverwrite(src, dst) {
+    if (isDirectory(src)) {
+        readdirSync(src).forEach(file =>
+            copyOverwrite(join(src, file), join(dst, file))
+        );
+    } else {
+        let dstFile, dstDir;
+        if (dst.endsWith("/")) {
+            dstDir = dst;
+            dstFile = join(dst, basename(src));
+        } else {
+            dstDir = dirname(dst);
+            dstFile = dst;
+        }
+        console.log("  copying", src, "to", dstFile);
+        mkdirSync(dstDir, { recursive: true });
+        copyFileSync(src, dstFile);
+    }
+}
+
+function main() {
+    const vendor = YAML.load("vendor.yaml");
+    const sortedLibs = Object.entries(vendor["libs"]).sort(
+        firstBy(([, lib]) => lib["priority"])
+    );
+    for (const [libName, lib] of sortedLibs) {
+        if (!lib["npmDir"] || !lib["files"]) {
+            continue;
+        }
+        console.log("Processing:", libName);
+        const distDir = `./lib/_npm/${lib["distDir"] || lib["npmDir"]}/`;
+        for (let file of lib["files"]) {
+            const allFilesInDir = file.endsWith("/*");
+            if (allFilesInDir) {
+                file = file.replace("/*", "");
+            }
+
+            srcPattern = `./node_modules/${lib["npmDir"]}/${file}`;
+            if (isDirectory(srcPattern) && !allFilesInDir) {
+                copyOverwrite(srcPattern, join(distDir, file));
+            } else {
+                copyOverwrite(srcPattern, distDir);
+            }
+        }
+    }
+
+    require("./vendor_license");
+}
+
+main();
