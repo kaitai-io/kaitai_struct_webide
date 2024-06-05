4234f88eaac4fe7839456dc724b5622a8c75cf47
Sylwester Wroblewski <swrobl@proton.me>
2024-06-05 21:16:52 +0200

    Removed V2 from the project

--- src/v2/autorefresh2.ts
+++
@@ -1,6 +0,0 @@
-function checkModifications2() {
-    fetch("/onchange").then(_ => location.reload(true), _ => setTimeout(checkModifications2, 750));
-}
-
-if(location.hostname === "127.0.0.1")
-    checkModifications2();
--- src/v2/utils/Conversion.ts
+++
@@ -1,9 +0,0 @@
-export class Conversion {
-    static utf8BytesToStr(bytes: ArrayBufferLike) {
-        return new TextDecoder("utf-8").decode(new Uint8Array(bytes));
-    }
-
-    static strToUtf8Bytes(str: string) {
-        return new TextEncoder().encode(str).buffer;
-    }
-}
