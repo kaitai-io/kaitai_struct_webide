99203ce66924c2e3e6760cd24694ee7cf20aeaf7
Sylwester Wroblewski <swrobl@proton.me>
2024-06-08 20:50:10 +0200

    Improvements to extensions.ts

--- src/extensions.ts
+++ src/extensions.ts
@@ -5,15 +5,12 @@ interface Array<T> {
 
 /*
 * This method needs to be initialized, otherwise JSTree has some internal problem.
-* Problem is not thrown or logged to the console, but localStorage fileTree stops loading on page refresh.
-* What is more this function is never probably never called since console log is not logged.
+* Problem is not thrown or logged to the console, but "Local storage" fileTree stops loading on page refresh.
+* What is more, is that this function is probably never called since console log is not executed.
 * What a freaking joke...
 * */
-Array.prototype.remove = function <T>(item: T) {
-    console.log("Why are we still here? Just to suffer");
-    for (let i = this.length; i--;) {
-        if (this[i] === item) {
-            this.splice(i, 1);
-        }
-    }
-};
+if (!Array.prototype.remove) {
+    Array.prototype.remove = function <T>(item: T) {
+        console.log("Why are we still here? Just to suffer");
+    };
+}
