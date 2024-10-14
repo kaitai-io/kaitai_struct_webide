88842d7fc8f076825592aeb4bc91910344f0664e
Sylwester Wroblewski <swrobl@proton.me>
2024-10-14 11:30:07 +0200

    Fixing ugly scrollbars in chrome

--- src/v1/app.ts
+++ src/v1/app.ts
@@ -3,6 +3,7 @@ import "./../../css/contextmenu.css";
 import "golden-layout/src/css/goldenlayout-base.css";
 import "golden-layout/src/css/goldenlayout-dark-theme.css";
 import "./../../css/app.css";
+import "./../../css/scrollbars.css";
 import "jstree/dist/themes/default/style.min.css";
 import "jstree/dist/themes/default-dark/style.min.css";
 import "font-awesome/css/font-awesome.min.css";
