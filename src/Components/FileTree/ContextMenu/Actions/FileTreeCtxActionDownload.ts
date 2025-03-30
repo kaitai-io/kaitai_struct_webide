902a8afc8aac0968ede5fe62e4d62ac28c6c1fac
Sylwester Wroblewski <swrobl@proton.me>
2025-03-30 13:04:06 +0200

    Extended download functionality to allow for downloading folders from local storage

--- src/Components/FileTree/ContextMenu/Actions/FileTreeCtxActionDownload.ts
+++ src/Components/FileTree/ContextMenu/Actions/FileTreeCtxActionDownload.ts
@@ -4,19 +4,59 @@ import {h} from "vue";
 import {FileActionsWrapper} from "../../../../Utils/Files/FileActionsWrapper";
 import {useFileSystems} from "../../Store/FileSystemsStore";
 import {CloudArrowDownIcon} from "@heroicons/vue/16/solid";
+import {FILE_SYSTEM_TYPE_KAITAI} from "../../FileSystems/KaitaiFileSystem";
+import {FileSystemPath} from "../../FileSystemsTypes";
+import {FileToPack, ZipUtil} from "../../../../Utils/Files/ZipUtil";
+
+const mapFilesForPacking = async (getFile: (filePath: string) => Promise<string | ArrayBuffer>, rootPath: string, filePathsInNode: string[]): Promise<FileToPack[]> => {
+    const itemPathParts = rootPath.split("/");
+    const relativePath = itemPathParts[itemPathParts.length - 1];
+
+    const data: FileToPack[] = [];
+    for (const filePath of filePathsInNode) {
+        try {
+            const fullFilePath = `${relativePath}/${filePath}`;
+            const fileContent = await getFile(fullFilePath);
+            data.push({
+                path: fullFilePath,
+                data: fileContent
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    }
+    return data;
+};
 
 export const FileTreeCtxActionDownload = (item: TreeNodeDisplay): MenuItem => {
-    const action = async () => {
+    const isFile = [TreeNodeDisplayType.KSY_FILE, TreeNodeDisplayType.BINARY_FILE].indexOf(item.type) !== -1;
+
+    const downloadSingleFile = async () => {
         const store = useFileSystems();
         const fileContent = await store.getFile(item.storeId, item.fullPath);
         FileActionsWrapper.downloadFile(fileContent, item.fileName);
+    };
+
+    const packAndDownloadDirectory = async () => {
+        const store = useFileSystems();
+        const filePathsInNode = await store.listAllItemsInPath(FileSystemPath.of(item.storeId, item.fullPath));
+        const getFileFn = (filePath: string) => store.getFile(item.storeId, filePath);
+
+        const filesToPack = await mapFilesForPacking(getFileFn, item.fullPath, filePathsInNode);
+        FileActionsWrapper.downloadBlob(await ZipUtil.packZip(filesToPack), `kaitai-${Date.now()}.zip`);
+    };
+
+    const action = async () => {
+        isFile
+            ? await downloadSingleFile()
+            : await packAndDownloadDirectory();
     };
 
     return {
         label: "Download",
         onClick: action,
         customClass: "context-menu-item",
-        disabled: [TreeNodeDisplayType.KSY_FILE, TreeNodeDisplayType.BINARY_FILE].indexOf(item.type) === -1,
+        disabled: item.storeId === FILE_SYSTEM_TYPE_KAITAI && !isFile,
         icon: () => h(CloudArrowDownIcon),
     };
 };
--- src/Utils/Files/FileActionsWrapper.ts
+++ src/Utils/Files/FileActionsWrapper.ts
@@ -27,16 +27,17 @@ export class FileActionsWrapper {
     }
 
     public static downloadFile(data: ArrayBuffer | Uint8Array | string, filename: string) {
-        const a = document.createElement("a");
-        document.body.appendChild(a);
-        a.style.display = "none";
         const blob = new Blob([data], {type: "octet/stream"});
+        this.downloadBlob(blob, filename);
+    }
+
+    public static downloadBlob(blob: Blob, filename: string) {
+        const a = document.createElement("a");
         const url = window.URL.createObjectURL(blob);
         a.href = url;
         a.download = filename;
         a.click();
         window.URL.revokeObjectURL(url);
-        document.body.removeChild(a);
     }
 
     public static mapToProcessItems(files: FileList): IFileProcessItem[] {
